9061885a7646d01a2739bec4e28b433e124cf407
shrihari06 <shriharim6@gmail.com>
2021-07-23 18:31:31 +0530

    Added Eslint and webpack env's

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import './styles.css';
+import IMG from './react.png';
+import LOGO from './logo.svg';
+import { ClickCounter } from './ClickCounter';
+
 export const App = () => {
-  return <h1>React Typescript Webpack Starter Template</h1>
+  return <>
+    <h1>React Typescript Webpack Starter Template {process.env.NODE_ENV} {process.env.name}</h1>
+    <img src={IMG} alt='react-logo' />
+    <img src={LOGO} alt='react-logo' width="300" />
+    <ClickCounter />
+  </>
 }
---
+++ src/ClickCounter.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export const ClickCounter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <button
+      onClick={() => setCount(c => c + 1)}
+    >
+      Count {count}
+    </button>
+  )
+}
