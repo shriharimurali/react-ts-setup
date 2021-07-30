9c3f485cbfba0ec67f247b9b2a69caf70726fa6f
shrihari06 <shriharim6@gmail.com>
2021-07-30 12:14:10 +0530

    Add Prettier and eslint setup

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 export const ClickCounter = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <button
-      onClick={() => setCount(c => c + 1)}
-    >
-      Count {count}
-    </button>
-  )
+  const [count, setCount] = useState(0)
+  return <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
 }
