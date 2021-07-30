8830fbab5e2416ae2ce338f52e88767e1c27df6c
shrihari06 <shriharim6@gmail.com>
2021-07-30 12:57:44 +0530

    Add Bundle analyzer and copy plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,10 +1,13 @@
-const webpack = require('webpack');
+const webpack = require("webpack")
+const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: "production",
+  devtool: "source-map",
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Production Code'),
-    })
-  ]
-};
+      "process.env.name": JSON.stringify("Production Code"),
+    }),
+    new BundleAnalyzer(),
+  ],
+}
